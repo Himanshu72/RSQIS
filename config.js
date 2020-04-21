7c30ec428ec31fcaab1ae28e2f0f0d5fc6dbf3c2
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2020-04-21 18:59:40 +0530

    online db

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
   secret: "ilovethotho",
   email: "eventspot07@gmail.com",
   pass: "h4ck3d321",
-  DBname: "rsqis1",
-  DBuser: "root",
-  DBpass: "mysql",
-  host: "localhost",
+  DBname: "LhAbPBTNr8",
+  DBuser: "LhAbPBTNr8",
+  DBpass: "pPJ4SP7I1s",
+  host: "remotemysql.com",
   port: 3306,
 };
